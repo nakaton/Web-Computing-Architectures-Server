9ae20ede0613409f3705c2f44845a1614570f147
yya125 <yyd5586597>
2019-03-06 14:23:23 +1300

    Create new MVC file for yya125 functional api

--- app/routes/application.routes.js
+++ app/routes/application.routes.js
@@ -3,5 +3,4 @@ const application = require('../controllers/application.controller');
 module.exports = function (app) {
     app.route(app.rootUrl + '/venues')
         .get(application.getVenues);
-
 };
--- config/express.js
+++ config/express.js
@@ -19,7 +19,7 @@ module.exports = function () {
 
     // ROUTES
     require('../app/routes/backdoor.routes')(app);
-    require('../app/routes/application.routes')(app); //Route for functional api - yya125
+    require('../app/routes/application.routes')(app); // Route for functional api - yya125
 
     // DEBUG (you can remove this)
     app.get('/', function (req, res) {
