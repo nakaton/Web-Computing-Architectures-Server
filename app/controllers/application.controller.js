6e351794b7165ede8fe18f9f3201eb5080abd584
yya125 <yyd5586597>
2019-03-06 14:21:34 +1300

    Create new MVC file for yya125 functional api

--- app/controllers/application.controller.js
+++ app/controllers/application.controller.js
@@ -1,5 +1,5 @@
 //const Backdoor = require('../models/application.model');
 
 exports.getVenues = async function (req, res) {
-    res.send({ 'message': 'Hello World! venues' })
+    res.send({ 'message': 'Hello World! venues!' })
 };
--- app/routes/application.routes.js
+++ app/routes/application.routes.js
@@ -3,4 +3,5 @@ const application = require('../controllers/application.controller');
 module.exports = function (app) {
     app.route(app.rootUrl + '/venues')
         .get(application.getVenues);
+
 };
